9e2d11b41ea0149c730c6cc859c92f7d4436123b
Steven Mazanec <SMazanec@proexam.org>
2018-05-07 11:32:46 -0400

    thrones inspired

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,74 +1,85 @@
+var players = ["lebron james", "tristan thompson", "kevin love", "jr smith", "kyrie irving"];
+
 var wins = 0;
 var remainingGuesses = 10;
-var players = ["lebronjames", "tristanthompson", "kevinlove", "jrsmith", "kyrieirving"];
-var currentWordIndex;
+var currentWord = [];
 var answerArray = [];
 var lettersGuessed = [];
 var hasFinished = false;
+var hasWon = false;
 
-function resetGame() {
-    remainingGuesses = 10;
-    currentWordIndex = Math.floor(Math.random() * (players.length));
-    lettersGuessed = [];
+function chooseWord () {
+    currentWord = [];
     answerArray = [];
-    for (var i = 0; i < players[currentWordIndex].length; i++) {
-        answerArray.push = "_";
+    var selector = Math.floor(Math.random() * players.length);
+    var wordIndex = players[selector];
+    for (var i = 0; i < wordIndex.length; i++) {
+        currentWord.push(wordIndex.charAt(i));
+        answerArray.push(wordIndex.charAt(i));
     }
-};
-
-function updateDisplay() {
-    document.getElementById("wins").innerText = wins;
-    document.getElementById("currentWord").innerText = "";
-    for (var j = 0; j < answerArray.length; j++) {
-        document.getElementById("currentWord").innerText += answerArray[i];
-    }
-    document.getElementById("remainingGuesses").innerText = remainingGuesses;
-    document.getElementById("lettersGuessed").innerText = lettersGuessed;
-};
-
-function evaluateGuess(letter) {
-    var positions = [];
-    for (var k = 0; k < players[currentWordIndex].length; k++) {
-        if (players[currentWordIndex][k] === letter) {
-            positions.push[k];
-        }
-    }
-    if (positions.length <= 0) {
-        remainingGuesses--; 
-    } else {
-        for (var k = 0; k < positions.length; k++){
-            answerArray[positions[k]] === letter;
-        }
-    }
-};
-
-function checkWin () {
-    if (answerArray.indexOf("_") === -1) {
-        wins++;
-        hasFinished=true;
+    var targetWord = document.getElementById("currentWord");
+    for (var i = 0; i <currentWord.length; i++) {
+        var currentLetter = currentWord[i];
+        var newP = document.createElement("p");
+        newP.innerHTML = "_";
+        targetWord.appendChild(newP);
+        newP.setAttribute("class", "letterP");
     }
 }
+chooseWord();
 
-function makeGuess (letter) {
-    if (remainingGuesses > 0) {
-        if (lettersGuessed.indexOf(letter) === -1) {
-            lettersGuessed.push(letter);
-            evaluateGuess(letter);
-        }
-        else if (lettersGuessed.indexOf(letter) === 1) {
-            alert("You've already guessed " + letter);
-        }
-    }
-};
+document.onkeyup = function(event){
+    var userGuess = String.fromCharCode(event.keyCode).toLowerCase();
 
-document.onkeyup = function(event) {
-    if (hasFinished) {
-        resetGame();
-        hasFinished = false;
+    var alreadyGuessed = lettersGuessed.indexOf(userGuess);
+    if (alreadyGuessed >= 0) {
+        alert(" you have already guessed that letter");
+        return;
     }
     else {
-        makeGuess(event.key.toLowerCase());
-        updateDisplay;
-        checkWin;
     }
-};
+    lettersGuessed.push(userGuess);
+    var newLetterP = document.getElementById("lettersGuessed");
+    newLetterP.innerHTML = "<p>" + lettersGuessed + "</p>";
+    var isPartOfWord = currentWord.indexOf(userGuess);
+    var chosenWordP = document.getElementsByClassName("letterP");
+    var count = 0;
+    for (var i = 0; i <currentWord.length; i++) {
+        if (currentWord[i] === userGuess) {
+            count++;
+            chosenWordP[i].innerHTML = "<p>" + currentWord[i] + "</p>";
+        }
+    }
+    if (alreadyGuessed >= 0) {
+        for (var i = 0; i < count; i++) {
+            answerArray.pop()
+        }
+    }
+    else {
+        remainingGuesses--;
+        var remainingGuessesP = document.getElementById("remainingGuesses");
+        remainingGuessesP.innerHTML = "<p>" + remainingGuesses + "</p>";
+    }
+    if (answerArray.length === 0) {
+        hasFinished = true;
+        hasWon = true;
+        wins++;
+        var newWinP = document.getElementById("wins");
+        newWinP.innerHTML = "<p>" + wins + "</p>";
+    }
+    if (hasFinished === true && hasWon === true) {
+        lettersGuessed = [];
+        var newLetterP = document.getElementById("lettersGuessed");
+        newLetterP.innerHTML = "<p>" + lettersGuessed + "</p>";
+        var newResetP = document.getElementById("currentWord");
+        newResetP.innerHTML = " ";
+        var newResetRemainingP = document.getElementById("remainingGuesses");
+        newResetRemainingP.innerHTML = "<p>" + 10 + "</p>";
+        chooseWord();
+        remainingGuesses = 10;
+        hasFinished = false;
+        hasWon = false;
+    }
+    else {
+    }
+}
